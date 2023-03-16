2275b6fe4db575161a9638886ff922dce043ca4e
laytn <93426549+laytn@users.noreply.github.com>
2023-03-16 17:41:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import re
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
-# network 설정
-web3 = Web3(Web3.HTTPProvider('https://public-node-api.klaytnapi.com/v1/cypress'))
+# Network configuration
+mainnet = "rpc url"
+web3 = Web3(Web3.HTTPProvider(mainnet))
 
-# 디컴파일러 설정
+# Decompiler configuration
 clear = lambda : os.system('clear')
 clear()
 env = os.environ
@@ -15,11 +16,9 @@ newpath = r'/home/{username}/.local/bin'+env['PATH']
 env['PATH'] = newpath
 Contract_bytecode = web3.eth.get_code(web3.toChecksumAddress("0x93dd4a0b32b1e6495ec943d541a6f52e3cbcb834")).hex()
 
-# 바이트코드 디컴파일러
+# Bytecode decompiler
 def decompile(contractbytecode):
 #     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     r = subprocess.check_output(['panoramix',tokenbytecode],universal_newlines=True)
 #     r1 = str(ansi_escape.sub('', r))
     return r
-
-print(decompile(Contract_bytecode))
