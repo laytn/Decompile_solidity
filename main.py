85a3aaca10969e179efd67e2cd54a740955102d2
laytn <93426549+laytn@users.noreply.github.com>
2023-03-16 17:17:06 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+import re
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+# network 설정
+web3 = Web3(Web3.HTTPProvider('https://public-node-api.klaytnapi.com/v1/cypress'))
+
+# 디컴파일러 설정
+clear = lambda : os.system('clear')
+clear()
+env = os.environ
+newpath = r'/home/ethan/.local/bin'+env['PATH']
+env['PATH'] = newpath
+Token_bytecode = web3.eth.get_code(web3.toChecksumAddress("0x93dd4a0b32b1e6495ec943d541a6f52e3cbcb834")).hex()
+
+# 바이트코드 디컴파일러
+def decompile(tokenbytecode):
+    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    r = subprocess.check_output(['panoramix',tokenbytecode],universal_newlines=True)
+    r1 = str(ansi_escape.sub('', r))
+    return r1
+
+print(decompile(Token_bytecode))
